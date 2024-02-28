920e34a185d08cdd6987ab1acd5945481916797c
Tiemen Deroose <tiemen.deroose@student.hogent.be>
2024-02-28 17:51:57 +0100

    SDP2324VC2-36 Gebruiker testen aanvullen

--- src/service/AanmeldServiceMock.java
+++ src/service/AanmeldServiceMock.java
@@ -21,7 +21,9 @@ public class AanmeldServiceMock implements AanmeldService {
 	private List<Gebruiker> mockGebruikers = Arrays.asList(
 			new Gebruiker(1, Rol.LEVERANCIER, "eerste@hotmail.com", "1234", "Mark", true, adressen[0]),
 			new Gebruiker(2, Rol.LEVERANCIER, "tweede@hotmail.com", "password", "Mike", true, adressen[1]),
-			new Gebruiker(3, Rol.ADMINISTRATOR, "derde@hotmail.com", "123abc", "Joris", true, adressen[2])
+			new Gebruiker(3, Rol.ADMINISTRATOR, "derde@hotmail.com", "123abc", "Joris", true, adressen[2]),
+			new Gebruiker(4, Rol.KLANT, "vierde@hotmail.com", "321", "Jeff", true, adressen[1]),
+			new Gebruiker(5, Rol.LEVERANCIER, "vijfde@hotmail.com", "654", "Jos", false, adressen[2])
 			);
 	
 	@Override
@@ -31,10 +33,20 @@ public class AanmeldServiceMock implements AanmeldService {
 
 	@Override
 	public Gebruiker meldGebruikerAan(String emailadres, String wachtwoord) throws NoSuchElementException {
-		return mockGebruikers
+		Gebruiker gebruiker = mockGebruikers
 			.stream()
 			.filter((Gebruiker g) -> g.getEmail().equals(emailadres) && g.getWachtwoord().equals(wachtwoord))
 			.findFirst()
 			.get();
+		
+		if (!gebruiker.getIsActief()) {
+			throw new IllegalArgumentException("De gebruikersaccount is niet actief.");
+		}
+		
+		if (gebruiker.getRol() == Rol.KLANT) {
+			throw new IllegalArgumentException("De gebruiker mag geen klant zijn.");
+		}
+		
+		return gebruiker;
 	}
 }
