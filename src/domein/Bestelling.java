e0ee20946d1580016ed91926ca8b330b32ef1cfe
Jasper Vandenbroucke <jasper.vandenbroucke@student.hogent.be>
2024-02-28 22:16:30 +0100

    SDP2324VC2-45 BestellingTest uitgewerkt + Bestelling aangepast

--- src/domein/Bestelling.java
+++ src/domein/Bestelling.java
@@ -2,8 +2,11 @@ package domein;
 
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.EnumSet;
 import java.util.List;
 
 import javafx.beans.property.IntegerProperty;
@@ -33,28 +36,30 @@ public class Bestelling {
 	
 	//constructor
 	public Bestelling(int orderId, Date datumGeplaats, OrderStatus orderStatus, BetalingsStatus betalingStatus, Klant klant) {
-		this.orderId = orderId;
-		this.datumGeplaats = datumGeplaats;
-		this.orderStatus = orderStatus;
-		this.betalingStatus = betalingStatus;
+//		this.orderId = orderId;
+//		this.datumGeplaats = datumGeplaats;
+//		this.orderStatus = orderStatus;
+//		this.betalingStatus = betalingStatus;
 		setKlant(klant);
-		//Tableview
 		setOrderId(orderId);
 		setDatumGeplaats(datumGeplaats);
 		setOrderStatus(orderStatus);
 		setBetalingStatus(betalingStatus);
 	}
 
-	//getters
 	public String getKlantName() {
 		return klant.getName();
 	}
 	
 	private void setKlant(Klant klant) {
+		if (klant == null)
+			throw new IllegalArgumentException("Er is geen klant meegegeven");
 		this.klant = klant;
 	}
 
 	public void setProducten(List<Product> producten) {
+		if (producten.isEmpty())
+			throw new IllegalArgumentException("De bestelling bevat geen producten");
 		this.producten = producten;
 	}
 
@@ -62,21 +67,48 @@ public class Bestelling {
 		return this.producten;
 	}
 	
-	//voor tabelView
 	private void setOrderId(int oId) {
+		if (oId <= 0)
+			throw new IllegalArgumentException("OrderId moet strikt positief zijn");
 		orderID.set(oId);
+		orderId = oId;
+	}
+	
+	public int getOrderId() {
+		return this.orderId;
 	}
 
 	private void setDatumGeplaats(Date date) {
+		if (date.before(new Date()))
+			throw new IllegalArgumentException();
 		datum.set(date);
+		datumGeplaats = date;
+	}
+	
+	public Date getDatumGeplaatst() {
+		return this.datumGeplaats;
 	}
 
 	private void setBetalingStatus(BetalingsStatus bStatus) {
+		if (bStatus == null)
+			throw new IllegalArgumentException("Betalingsstatus is niet meegegeven");
 		betalingsstatus.set(bStatus);
+		betalingStatus = bStatus;
+	}
+	
+	public BetalingsStatus getBetalingsStatus() {
+		return this.betalingStatus;
 	}
 
 	private void setOrderStatus(OrderStatus oStatus) {
+		if (oStatus == null)
+			throw new IllegalArgumentException("Orderstatus is niet meegegeven");
 		orderstatus.set(oStatus);
+		orderStatus = oStatus;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return this.orderStatus;
 	}
 	
 	public IntegerProperty orderIdProperty() {
